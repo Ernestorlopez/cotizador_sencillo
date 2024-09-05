d138796d9358fd70f5f6e50e15c82540e9fc6702
Ernesto Ramón López <ernestorlopez@hotmail.com.ar>
2024-09-05 16:36:05 -0300

    Se agregaron funciones al formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,146 +1,313 @@
-$('document').ready(() => {
+$("document").ready(() => {
+  function notify(content, type = "success") {
+    let wrapper = $(".wrapper_notifications"),
+      id = Math.floor(Math.random() * 500 + 1),
+      notificacion =
+        '<div class="alert alert-' +
+        type +
+        '" id="noty_' +
+        id +
+        '">' +
+        content +
+        "</div>",
+      time = 5000;
 
-    function notify(content, type = 'success'){
-        let wrapper  = $('.wrapper_notifications'),
-        id          = Math.floor(Math.random()* 500 + 1),
-        notificacion = '<div class="alert alert-'+type+'" id="noty_'+id+'">'+content+'</div>',
-        time         = 5000;
+    //insertar en el contenedor la notificación
+    wrapper.append(notificacion);
 
-        //insertar en el contenedor la notificación
-        wrapper.append(notificacion);
+    //Timer para ocultar la notificación
+    setTimeout(function () {
+      $("#noty_" + id).remove();
+    }, time);
 
-        //Timer para ocultar la notificación
-        setTimeout(function(){
-            $('#noty_'+id).remove();
-        }, time);
+    return true;
+  }
 
-        return true;
+  //Cargar contenido de la cotización
+  function get_quote() {
+    let wrapper = $(".wrapper_quote"),
+      action = "get_quote_res",
+      nombre = $("#nombre"),
+      empresa = $("#empresa"),
+      email = $("#email");
 
-    } 
-
-    //Cargar contenido de la cotización
-    function get_quote(){
-        let wrapper = $('.wrapper_quote'),
-        action      = 'get_quote_res';
-        nombre      = $('#nombre'),
-        empresa     = $('#empresa'),
-        email       = $('#email');
-
-
-        $.ajax({
-            url: 'ajax.php',
-            type: 'get',
-            cache: false,
-            dataType: 'json',
-            data: {action},
-            beforeSend: function(){
-                wrapper.waitMe({});
-            }
-        }).done(res => {
-            if(res.status === 200){
-                nombre.val(res.data.quote.nombre);
-                empresa.val(res.data.quote.empresa);
-                email.val(res.data.quote.email);
-                wrapper.html(res.data.html);
-            } else {
-                wrapper.html(res.msg);
-            }
-        }).fail(err => {
-            wrapper.html('Ocurrió un error, por favor recarga la página');
-        }).always(() => {
-            wrapper.waitMe('hide');
-        });
-    }
-    get_quote();
-
-    //Función para agregar un concepto a la cotización
-    $('#add_to_quote').on('submit', add_to_quote);
-    function add_to_quote(e) {
-        e.preventDefault();
-
-        let form     = $('#add_to_quote'),
-        action       = 'add_to_quote',
-        data         = new FormData(form.get(0)),
-        errors       = 0;
-
-        //Agregar la accion al objeto data
-        data.append('action', action);
-
-        //Validar concepto
-        let concepto = $('#concepto').val(),
-        precio       = parseFloat($('#precio_unitario').val());
-
-        if(concepto.length < 5){
-            notify('Ingresa un concepto válido por favor.', 'danger');
-            errors++;
+    $.ajax({
+      url: "ajax.php",
+      type: "get",
+      cache: false,
+      dataType: "json",
+      data: { action },
+      beforeSend: function () {
+        wrapper.waitMe({});
+      },
+    })
+      .done((res) => {
+        if (res.status === 200) {
+          nombre.val(res.data.quote.nombre);
+          empresa.val(res.data.quote.empresa);
+          email.val(res.data.quote.email);
+          wrapper.html(res.data.html);
+        } else {
+          wrapper.html(res.msg);
         }
+      })
+      .fail((err) => {
+        wrapper.html("Ocurrió un error, por favor recarga la página");
+      })
+      .always(() => {
+        wrapper.waitMe("hide");
+      });
+  }
+  get_quote();
 
-        //Validar precios
-        if(precio < 10){
-            notify('Por favor ingrese un precio mayor a $10.', 'danger');
-            errors++;
-        }
+  //Función para agregar un concepto a la cotización
+  $("#add_to_quote").on("submit", add_to_quote);
+  function add_to_quote(e) {
+    e.preventDefault();
 
-        if(errors > 0){
-            notify('Completa el formulario.', 'danger');
-            return false;
-        }
+    let form = $("#add_to_quote"),
+      action = "add_to_quote",
+      data = new FormData(form.get(0)),
+      errors = 0;
 
-        $.ajax({
-            url        : 'ajax.php',
-            type       : 'POST',
-            dataType   : 'json',
-            cache      : false,
-            processData: false,
-            contentType: false,
-            data       : data,
-            beforeSend : ()=>{
-                form.waitMe();
-            }
-        }).done(res => {
-            if(res.status === 201){
-                notify(res.msg);
-                form.trigger('reset');
-                get_quote();
-            } else {
-                notify(res.msg, 'danger');
-            }
-        }).fail(err => {
-            notify('Hubo un problema con la petición, intenta de nuevo.', 'danger');
-            form.trigger('reset');
-        }).always(() => {
-            form.waitMe('hide');
-        })
-        
+    //Agregar la accion al objeto data
+    data.append("action", action);
+
+    //Validar concepto
+    let concepto = $("#concepto").val(),
+      precio = parseFloat($("#precio_unitario").val());
+
+    if (concepto.length < 5) {
+      notify("Ingresa un concepto válido por favor.", "danger");
+      errors++;
     }
 
-    //Función para reiniciar la cotización
-    $('.restart_quote').on('click', restart_quote);
-    function restart_quote(e){
-        e.preventDefault;
-
-        let button = $(this),
-        action     ='restart_quote';
-
-        if(!confirm('¿Estás seguro?')) return false;
-
-        //Petición
-        $.ajax({
-            url     : 'ajax.php',
-            type    : 'POST',
-            dataType: 'json',
-            data    :{action}
-        }).done(res => {
-            if(res.status === 200){
-                notify(res.msg);
-                get_quote();
-            } else {
-                notify(res.msg, 'danger');
-            }
-        }).fail(err => {
-            notify('Hubo un problema con la petición.', 'danger');
-        }).always(() => {
-
-        });
+    //Validar precios
+    if (precio < 10) {
+      notify("Por favor ingrese un precio mayor a $10.", "danger");
+      errors++;
     }
+
+    if (errors > 0) {
+      notify("Completa el formulario.", "danger");
+      return false;
+    }
+
+    $.ajax({
+      url        : "ajax.php",
+      type       : "POST",
+      dataType   : "json",
+      cache      : false,
+      processData: false,
+      contentType: false,
+      data       : data,
+      beforeSend : () => {
+        form.waitMe();
+      },
+    })
+      .done((res) => {
+        if (res.status === 201) {
+          notify(res.msg);
+          form.trigger("reset");
+          get_quote();
+        } else {
+          notify(res.msg, "danger");
+        }
+      })
+      .fail((err) => {
+        notify("Hubo un problema con la petición, intenta de nuevo.", "danger");
+        form.trigger("reset");
+      })
+      .always(() => {
+        form.waitMe("hide");
+      });
+  }
+
+  //Función para reiniciar la cotización
+  $(".restart_quote").on("click", restart_quote);
+  function restart_quote(e) {
+    e.preventDefault;
+
+    let button = $(this),
+      action = "restart_quote";
+
+    if (!confirm("¿Estás seguro?")) return false;
+
+    //Petición
+    $.ajax({
+      url: "ajax.php",
+      type: "POST",
+      dataType: "json",
+      data: { action },
+    })
+      .done((res) => {
+        if (res.status === 200) {
+          notify(res.msg);
+          get_quote();
+        } else {
+          notify(res.msg, "danger");
+        }
+      })
+      .fail((err) => {
+        notify("Hubo un problema con la petición.", "danger");
+      })
+      .always(() => {});
+  }
+
+  //Función para eliminar elementos individuales
+  $("body").on("click", ".delete_concept", delete_concept);
+  function delete_concept(e) {
+    e.preventDefault();
+
+    let button = $(this),
+      id = button.data("id"),
+      action = "delete_concept";
+
+    if (!confirm("¿Estás seguro?")) return false;
+
+    //Petición
+    $.ajax({
+      url: "ajax.php",
+      type: "POST",
+      dataType: "json",
+      data: { action, id },
+      beforeSend: () => {
+        $("body").waitMe();
+      },
+    })
+      .done((res) => {
+        if (res.status === 200) {
+          notify(res.msg);
+          get_quote();
+        } else {
+          notify(res.msg, "danger");
+        }
+      })
+      .fail((err) => {
+        notify("Hubo un problema con la petición", "danger");
+      })
+      .always(() => {
+        $("body").waitMe("hide");
+      });
+  }
+
+  //Función para cargar un sólo concepto
+  $("body").on("click", ".edit_concept", edit_concept);
+  function edit_concept(e) {
+    e.preventDefault();
+
+    let button = $(this),
+      id = button.data("id"),
+      action = "edit_concept",
+      wrapper_update_concept = $(".wrapper_update_concept"),
+      form_update_concept = $("#save_concept");
+
+    //Petición
+    $.ajax({
+      url: "ajax.php",
+      type: "POST",
+      dataType: "json",
+      data: { action, id },
+      beforeSend: () => {
+        $("body").waitMe();
+      },
+    })
+      .done((res) => {
+        if (res.status === 200) {
+          $("#id_concepto", form_update_concept).val(res.data.id);
+          $("#concepto", form_update_concept).val(res.data.concept);
+          $(
+            '#tipo option[value="' + res.data.type + '"]',
+            form_update_concept
+          ).attr("selected", true);
+          $("#cantidad", form_update_concept).val(res.data.quantity);
+          $("#precio_unitario", form_update_concept).val(res.data.price);
+          wrapper_update_concept.fadeIn();
+          notify(res.msg);
+        } else {
+          notify(res.msg, "danger");
+        }
+      })
+      .fail((err) => {
+        notify("Hubo un problema con la petición", "danger");
+      })
+      .always(() => {
+        //$("body").waitMe("hide");
+      });
+  }
+
+  //Función para guardar cambios en elemento
+  $("#save_concept").on("submit", save_concept);
+  function save_concept(e) {
+    e.preventDefault();
+
+    let form                 = $("#save_concept"),
+    action                   = "save_concept",
+    data                     = new FormData(form.get(0)),
+    wrapper_update_concept    = $(".wrapper_update_concept"),
+    errors                   = 0;
+
+    //Agregar la acción al objeto data
+    data.append("action", action);
+
+    //Validar el concepto
+    let concepto = $("#concepto", form).val(),
+      precio = parseFloat($("#precio_unitario", form).val());
+
+    if (concepto.length < 5) {
+      notify("Ingresa un concepto válido.", "danger");
+      errors++;
+    }
+
+    //Validar el precio
+    if (precio < 10) {
+      notify("Por favor ingresa un precio mayor a $10", "danger");
+    }
+
+    if (errors > 0) {
+      notify("Completa el formulario", "danger");
+      return false;
+    }
+
+    $.ajax({
+      url         : 'ajax.php',
+      type        : 'POST',
+      dataType    : 'json',
+      cache       : false,
+      processData : false,
+      contentType : false,
+      data        : data,
+      beforeSend  : () => {
+        form.waitMe();
+      }
+    }).done(res => {
+      if(res.status === 200){
+        form.trigger('reset');
+        wrapper_update_concept.fadeOut();
+        notify(res.msg);
+        get_quote();
+      } else {
+        notify(res.msg, 'danger');
+      }
+    }).fail(err => {
+      notify('Hubo un problema con la petición, intenta de nuevo.', 'danger');
+      wrapper_update_concept.fadeOut();
+      form.trigger('reset');
+    }).always(() => {
+      form.waitMe('hide');
+    })
+
+  }
+
+  //Cancel edit
+  $('#cancel_edit').on('click', (e) => {
+    e.preventDefault();
+
+    let button = $(this),
+    wrapper    = $('.wrapper_update_concept'),
+    form       = $('#save_concept');
+
+    wrapper.fadeOut();
+    form.trigger('reset');
+  });
 });
--- templates/views/indexView.php
+++ templates/views/indexView.php
@@ -61,12 +61,50 @@
                             </div>
                         </div>
                         <button class="btn btn-success" type="submit">Agregar concepto</button>
-                        <button class="btn btn-danger"  type="reset" >Cancelar</button>
+                        <button class="btn btn-danger" type="reset">Cancelar</button>
                     </form>
                 </div>
             </div>
         </div>
         <div class="col-lg-4 col-12">
+            <div class="wrapper_update_concept" style="display: none;">
+                <div class="card mb-2">
+                    <div class="card-header">Editar concepto</div>
+                    <div class="card-body">
+                        <form id="save_concept" method="POST">
+                            <input type="hidden" name="id_concepto" id="id_concepto">
+                            <div class="form-group row pb-4">
+                                <div class="col-3">
+                                    <label for="concepto">Concepto</label>
+                                    <input type="text" class="form-control" id="concepto" name="concepto" placeholder="Artículo" required>
+                                </div>
+                                <div class="col-3">
+                                    <label for="tipo">Tipo de producto</label>
+                                    <select name="tipo" id="tipo" class="form-control">
+                                        <option value="producto">Producto</option>
+                                        <option value="servicio">Servicio</option>
+                                    </select>
+                                </div>
+                                <div class="col-3">
+                                    <label for="cantidad">Cantidad</label>
+                                    <input type="number" class="form-control" id="cantidad" name="cantidad" min="1" max="99999" value="1" required>
+                                </div>
+                                <div class="col-3">
+                                    <label for="precio_unitario">Precio unitario</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">$</span>
+                                        </div>
+                                        <input type="text" class="form-control" id="precio_unitario" name="precio_unitario" placeholder="0,00" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="btn btn-success" type="submit">Guardar cambios</button>
+                            <button class="btn btn-danger" type="reset" id="cancel_edit">Cancelar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
             <div class="card">
                 <div class="card-header">Resumen de cotización <button class="btn btn-danger float-end restart_quote">Reiniciar</button></div>
                 <div class="card-body wrapper_quote">
